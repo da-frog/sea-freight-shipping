577f34e31f06b0ec505b6a1eefc694a222eda44c
Tetach Rattanavikran <47658385+theethaj@users.noreply.github.com>
2020-05-01 23:51:14 +0700

    add check_digits and rand_num

---
+++ check_n_rand/check_digits.py
@@ -0,0 +1,15 @@
+number = int(input("Please enter a sereal number: "))
+z6 = number % 10
+number = number//10
+z5 = number % 10
+number = number//10
+z4 = number % 10
+number = number//10
+z3 = number % 10
+number = number//10
+z2 = number % 10
+number = number//10
+z1 = number
+sum = z1+(2*z2)+(3*z3)+(4*z4)+(5*z5)+(6*z6)
+checkdigit = sum%11
+print("\nCheckdigit:", checkdigit)
