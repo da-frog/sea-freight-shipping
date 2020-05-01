4b52a21226e1c5cd4cb9afc6094f463b35dad88c
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-01 23:27:33 +0700

    Refactor getting port details

--- scrape/port_detail.py
+++ scrape/port_detail.py
@@ -1,8 +1,7 @@
+from typing import Callable
+
 import requests
 from bs4 import BeautifulSoup
-
-res = requests.get('https://www.searates.com/port/durres_al.htm')
-soup = BeautifulSoup(res.text, 'html.parser')
 
 
 def find_address(soup: BeautifulSoup):
@@ -11,13 +10,37 @@ def find_address(soup: BeautifulSoup):
     return address
 
 
-def find_fax(soup: BeautifulSoup):
-    f = soup.find(**{'class': 'table table-bordered'}).find(string='Fax:').parent
-    fax = str(f.next_sibling.next_sibling)[4:-5]
-    return fax
+def get_port_detail(soup: BeautifulSoup) -> dict:
+    minsoup = soup.find(id='port_det')
+    dct = {}
+    for query, key, func in [
+        ('Port Authority:', None, None),
+        ('Phone:', None, None),
+        ('Fax:', None, None),
+        ('800 Number:', None, None),
+        ('Email:', None, None),
+        ('Web Site:', None, None),
+        ('Latitude:', None, None),
+        ('Longitude:', None, None),
+        ('UN/LOCODE:', None, None),
+        ('Port Type:', None, None),
+        ('Port Size:', None, None),
+        ('Max Draft:', None, None),
+    ]:
+        value = minsoup.find(string=query).parent.next_sibling.next_sibling.string
+        if func is not None:
+            assert isinstance(func, Callable)
+            value = func(value)
+        if key is not None:
+            dct[key] = value
+        else:
+            dct[query] = value
+    dct['Address'] = find_address(soup)
+    return dct
 
-address = find_address(soup)
-fax = find_fax(soup)
 
-print(address)
-print(fax)
+if __name__ == '__main__':
+    res = requests.get('https://www.searates.com/port/durres_al.htm')
+    soup = BeautifulSoup(res.text, 'html.parser')
+    detail = get_port_detail(soup)
+    print(detail)
