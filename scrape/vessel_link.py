4f5bcd60672bd4ca45c6beca37e7919df1757c16
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-02 00:53:11 +0700

    Refactor find_link()

--- scrape/vessel_link.py
+++ scrape/vessel_link.py
@@ -2,25 +2,24 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 
+def get_page(idx: int) -> str:
+    if idx == 1:
+        return ""
+    else:
+        return f"?page={idx}"
+
+
 def find_link(page):
     browser = webdriver.Chrome(executable_path=r'C:\Users\User\Desktop\Driver\chromedriver.exe')
     i = 1
     text = open("Ship_link.txt", "w")
     while i <= page:
-        if i == 1:
-            browser.get("https://www.vesselfinder.com/vessels")
-            soup = BeautifulSoup(browser.page_source, "html.parser")
-            answer = soup.find_all("a", {"class": "ship-link"})
-            for a in answer:
-                text.write("https://www.vesselfinder.com" + a["href"] + "\n")
-            i += 1
-        else:
-            browser.get(f"https://www.vesselfinder.com/vessels?page={i}")
-            soup = BeautifulSoup(browser.page_source, "html.parser")
-            answer = soup.find_all("a", {"class": "ship-link"})
-            for a in answer:
-                text.write("https://www.vesselfinder.com" + a["href"] + "\n")
-            i += 1
+        browser.get('https://www.vesselfinder.com/vessels' + get_page(i))
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+        answer = soup.find_all("a", {"class": "ship-link"})
+        for a in answer:
+            text.write("https://www.vesselfinder.com" + a["href"] + "\n")
+        i += 1
 
 
 find_link(60)
