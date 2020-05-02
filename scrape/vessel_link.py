911411be2b4164d61f337b1964bc5a8cd25503b1
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-02 21:04:24 +0700

    Move data to data folder

--- scrape/vessel_link.py
+++ scrape/vessel_link.py
@@ -12,7 +12,7 @@ def get_page(idx: int) -> str:
 def find_link(page):
     browser = webdriver.Chrome(executable_path=r'C:\Users\User\Desktop\Driver\chromedriver.exe')
     i = 1
-    text = open("Ship_link.txt", "w")
+    text = open("../data/Ship_link.txt", "w")
     while i <= page:
         browser.get('https://www.vesselfinder.com/vessels' + get_page(i))
         soup = BeautifulSoup(browser.page_source, "html.parser")
