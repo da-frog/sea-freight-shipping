dd8aa5dd7be3ffba2d7a1caa3a6d015e896a17ed
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-03 21:08:17 +0700

    Add `read_csv()` utility function

--- utils.py
+++ utils.py
@@ -1,3 +1,15 @@
+from typing import List, Dict
+import csv
+
+
+def read_csv_file(filename: str, *, encoding: str = 'utf-8') -> List[Dict]:
+    """ Reads a csv file and return a list of dictionaries. """
+    data = []
+    with open(filename, 'r', encoding=encoding) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+    return data
 
 
 def trim_file(file: str):
