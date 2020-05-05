6f2aa0695647c9a14136d33102a0a29db193079e
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-05 15:40:52 +0700

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 from typing import List, Dict, Any, TypeVar, Callable
 import csv
 import json
+from datetime import date, timedelta, datetime
 from abc import ABC, abstractmethod
 from numbers import Number
 
@@ -70,7 +71,37 @@ class Address(BaseModel):
         return Location(self.latitude, self.longitude)
 
 
+class Vehicle(BaseModel):
+    instances = []
+    fields = (
+        'Vehicle Key',
+        ('Vehicle Speed (km/h)', 'vehicle_speed_kmh'),
+        'Vehicle Type',
+        'Vehicle Name',
+        'Vehicle Capacity',
+        'Vehicle Builder',
+        'Vehicle Fuel Usage per Day',
+        'Current Latitude',
+        'Current Longitude'
+    )
+
+    def __init__(self):
+        self.vehicle_speed_kmh: float = None,
+        self.vehicle_type: str = '',
+        self.vehicle_name: str = '',
+        self.vehicle_capacity: float  = None,
+        self.vehicle_builder: str = '',
+        self.vehicle_fuel_usage_per_day: float = None,
+        self.current_latitude: str = '',
+        self.current_longitude: str = '',
+
+    @property
+    def current_position(self) -> Location:
+        return Location(self.current_latitude, self.current_longitude)
+
+
 class Port(BaseModel):
+    instances = []
     fields = (
         'Port Key',
         'Address Key',
@@ -112,6 +143,12 @@ class Port(BaseModel):
 
 class Leg(BaseModel):
     instances = []
+    fields = (
+        'Leg Key',
+        'Origin Port Key',
+        'Destination Port Key',
+        'Leg Miles',
+    )
 
     def __init__(self):
         self.origin_port_key: int = None
@@ -126,11 +163,84 @@ class Leg(BaseModel):
         return Port.get_instance_by_key(self.destination_port_key)
 
     @property
-    def leg_miles(self):
-        return km_to_mile(self.origin_port.location.calculate_distance(self.destination_port.location))
+    def leg_kms(self) -> float:
+        return self.origin_port.location.calculate_distance(self.destination_port.location)
 
-    def get_leg_fees(self, ship_speed_knots: Number):
-        return
+    @property
+    def leg_miles(self) -> float:
+        return km_to_mile(self.leg_kms)
+
+    def get_leg_time(self, ship_speed_knots: float) -> timedelta:
+        return timedelta(hours=self.leg_kms / knots_to_kmh(ship_speed_knots))
+
+    def get_leg_fees(self, ship_speed_knots: float, ship_fee_per_day: float) -> float:
+
+        return self.get_leg_time(ship_speed_knots).days * ship_fee_per_day
+
+
+class LegSchedule(BaseModel):
+    instances = []
+    fields = (
+        'Leg Schedule Key',
+        'Leg Key',
+        'Scheduled Departure Date',
+        'Scheduled Arrival Date',
+        'Actual Departure Date',
+        'Actual Departure Date',
+    )
+
+    def __init__(self):
+        self.leg_key: int = None
+        self.scheduled_departure_date: date = None
+        self.scheduled_arrival_date: date = None
+        self.actual_departure_date: date = None
+        self.actual_arrival_date: date = None
+
+    @property
+    def leg_schedule_key(self) -> int:
+        return self.key
+
+    @property
+    def departure_delay(self) -> timedelta:
+        return self.actual_departure_date - self.scheduled_departure_date
+
+    @property
+    def arrival_delay(self) -> timedelta:
+        return self.actual_arrival_date - self.scheduled_arrival_date
+
+
+class Voyage(BaseModel):
+    instances = []
+    fields = (
+        'Voyage Key',
+        'Leg Bridge Key',
+        'Origin Port Key',
+        'Destination Port Key',
+    )
+
+    def __init__(self):
+        self.leg_bridge_key: int = None
+        self.port_keys: List[int] = []
+
+    @property
+    def ports(self) -> List[Port]:
+        return list(map(Port.get_instance_by_key, self.port_keys))
+
+    @ports.setter
+    def ports(self, ports: List[Port]):
+        self.port_keys = list(map(lambda port: port.port_key, ports))
+
+    @property
+    def origin_port_key(self) -> int:
+        return self.port_keys[0]
+
+    @property
+    def destination_port_key(self) -> int:
+        return self.port_keys[-1]
+
+    @property
+    def total_time(self):
+
 
 
 class VoyageSchedule:
