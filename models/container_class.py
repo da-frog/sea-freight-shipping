46555aae09a6d6b54f31e3a643f8fab96bff1593
Punyawee Srithonhkerd <tonkla9874@gmail.com>
2020-05-05 20:15:21 +0700

    fix value and typo in container

--- models/container_class.py
+++ models/container_class.py
@@ -7,26 +7,21 @@ class ContainerClass(BaseModel):
         'Container Class Key',
         'ISO Size Type',
         'Tare Weight',
-        'Inside',
-        'Length',
-        'Inside',
-        'Width',
-        'Inside',
-        'Height',
+        'Inside_Length',
+        'Inside_Width',
+        'Inside_Height',
         'Capacity',
     )
 
     def __init__(self):
         self.iso_size_type: str = ''
         self.tare_weight: float = None
-        self.inside: float = None
-        self.length: float = None
-        self.inside: float = None
-        self.width: float = None
-        self.inside: float = None
-        self.height: float = None
+        self.inside_length: float = None
+        self.inside_width: float = None
+        self.inside_height: float = None
         self.capacity: float = None
 
     @property
     def container_class_key(self) -> int:
         return self.key
+
