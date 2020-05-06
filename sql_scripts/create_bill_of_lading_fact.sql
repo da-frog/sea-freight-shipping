6e24dbfea6f6885838c9155d08a84af6fee26e5d
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-06 21:05:00 +0700

    Delete create_bill_of_lading_fact.sql

--- sql_scripts/create_bill_of_lading_fact.sql
+++
@@ -1,6 +0,0 @@
-IF (SELECT OBJECT_ID('dbo.BillOfLadingFact')) > 0
-    DROP TABLE dbo.BillOfLadingFact;
-
-CREATE TABLE BillOfLadingFact (
-    [Bill-of-Lading Key]    int IDENTITY (1,1) NOT NULL
-);
