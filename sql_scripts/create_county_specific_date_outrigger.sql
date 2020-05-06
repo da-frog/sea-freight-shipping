ecaf5bd4402aadeaf2a4d8e8bb3fc80c77ef6911
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-06 20:06:16 +0700

    Add basic sql script for date outrigger

---
+++ sql_scripts/create_county_specific_date_outrigger.sql
@@ -0,0 +1,17 @@
+USE FreightShippingDW;
+
+IF OBJECT_ID ('dbo.CountrySpecificDateOutrigger', 'U') IS NOT NULL
+    DROP TABLE CountrySpecificDateOutrigger;
+GO
+CREATE TABLE CountrySpecificDateOutrigger
+(
+    [Date Key]                  int NOT NULL,
+    [Country Key]               int NOT NULL,
+    [Country Name]              nvarchar(50),
+    [Civil Name]                nvarchar(50),
+    [Civil Holiday Flag]        nvarchar(50),
+    [Religious Holiday Flag]    nvarchar(50),
+    [Religious Holiday Name]    nvarchar(50),
+    [Weekday Indicator]         nvarchar(50),
+    [Season Name]               nvarchar(50),
+);
