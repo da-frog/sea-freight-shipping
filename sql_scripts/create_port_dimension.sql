1c2d93820b9515da4241360f9c685f42aa2d4aca
magmagcup <47655424+magmagcup@users.noreply.github.com>
2020-05-06 20:14:02 +0700

    Edit port dimension

--- sql_scripts/create_port_dimension.sql
+++ sql_scripts/create_port_dimension.sql
@@ -4,10 +4,9 @@ IF (SELECT OBJECT_ID('dbo.PortDim')) > 0
 CREATE TABLE PortDim (
     [Port Key]           int IDENTITY (1,1) NOT NULL,
     [Port Name]          nvarchar(50),
+    [Port Type]          nvarchar(50),
     [Port Country]       nvarchar(50),
     [Port City]          nvarchar(50),
-    [Port Address]       nvarchar(500),
+    [Port Address]       nvarchar(100),
     [Port Telephone]     int,
-    [Port Opening Hour]  int,
-    [Port Closing Hour]  int,
 );
