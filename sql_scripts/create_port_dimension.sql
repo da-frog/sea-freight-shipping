d5ca167b50fa302f7e194e58d791d5d0493d2f60
magmagcup <47655424+magmagcup@users.noreply.github.com>
2020-05-06 21:19:14 +0700

    Change port dim to port dimension

--- sql_scripts/create_port_dimension.sql
+++ sql_scripts/create_port_dimension.sql
@@ -1,9 +1,9 @@
 USE FreightShippingDW;
 
-IF OBJECT_ID ('dbo.PortDim', 'U') IS NOT NULL
-    DROP TABLE PortDim;
+IF OBJECT_ID ('dbo.PortDimension', 'U') IS NOT NULL
+    DROP TABLE PortDimension;
 GO
-CREATE TABLE PortDim (
+CREATE TABLE PortDimension (
     [Port Key]           int IDENTITY (1,1) NOT NULL,
     [Port Name]          nvarchar(50),
     [Port Type]          nvarchar(50),
