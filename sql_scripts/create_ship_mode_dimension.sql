1f2d67207b57ca8b8d4d52633f4cad5dd31b34f1
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-06 21:35:09 +0700

    Change datatype from float -> decimal

--- sql_scripts/create_ship_mode_dimension.sql
+++ sql_scripts/create_ship_mode_dimension.sql
@@ -8,10 +8,9 @@ CREATE TABLE ShipModeDimension
     [Ship Mode Key]          int IDENTITY (1,1) NOT NULL PRIMARY KEY,
     [Vehicle Type]           nvarchar(50),
     [Vehicle Name]           nvarchar(100),
-    [Vehicle Capacity]       float,
-    [Vehicle Speed (km/h)]   float,
-    [Vehicle Speed (mile/h)] float,
+    [Vehicle Capacity]       decimal,
+    [Vehicle Speed (km/h)]   decimal,
+    [Vehicle Speed (mile/h)] decimal,
     [Vehicle Builder]        nvarchar(100),
-    [Fuel cost per mile]     float,
-    [Fuel cost per km]       float
+    [Fuel cost per day]      decimal,
 );
