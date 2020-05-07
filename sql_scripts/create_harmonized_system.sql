1f9d20e3824afeb2bed052121c018856aa7294e9
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-07 23:49:36 +0700

    Update create_harmonized_system.sql

--- sql_scripts/create_harmonized_system.sql
+++ sql_scripts/create_harmonized_system.sql
@@ -1,14 +1,14 @@
-USE FreightShippingDW;
+USE FreightShipping;
 
 IF OBJECT_ID('dbo.HarmonizedSystem', 'U') IS NOT NULL
-    DROP TABLE HarminizedSystem;
+    DROP TABLE HarmonizedSystem;
 GO
 
 CREATE TABLE HarmonizedSystem
 (
-    section     varchar(5)   NOT NULL,
-    hscode      varchar(6)   NOT NULL,
-    description varchar(255) NOT NULL,
-    parent      varchar(5)   NOT NULL,
-    level       tinyint      NOT NULL,
+    section     varchar(5)    NOT NULL,
+    hscode      varchar(6)    NOT NULL,
+    description nvarchar(255) NOT NULL,
+    parent      varchar(5)    NOT NULL,
+    level       tinyint       NOT NULL,
 );
