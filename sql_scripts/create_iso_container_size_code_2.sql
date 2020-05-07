23008a2349c264a0b4407e8ec1999bf89bea73bc
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-07 23:50:30 +0700

    Update create_iso_container_size_code_2.sql

--- sql_scripts/create_iso_container_size_code_2.sql
+++ sql_scripts/create_iso_container_size_code_2.sql
@@ -1,14 +1,14 @@
-USE FreightShippingDW;
+USE FreightShipping;
 
 IF OBJECT_ID('dbo.ISOSizeCode2', 'U') IS NOT NULL
     DROP TABLE ISOSizeCode2;
 GO
-CREATE TABLE ISOSizeCode2
 
+CREATE TABLE ISOSizeCode2
 (
-[Container height (mm)] int,
-[Container height (ft)] int,
-[Container height (in)] int,
-[Code character]		nvarchar(255),
-[Container width (mm)]	int,
+    [Container height (mm)] int,
+    [Container height (ft)] int,
+    [Container height (in)] int,
+    [Code character]		nvarchar(1),
+    [Container width (mm)]	int,
 );
