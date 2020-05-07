984e0513ea925363c7a3efc240e440050d5240d5
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-08 03:39:25 +0700

    Add printing for loading

--- models/__init__.py
+++ models/__init__.py
@@ -28,13 +28,21 @@ def load_database(dataset: str = None, mode: str = 'csv'):
     else:
         raise AssertionError('no such mode \'' + mode + "'")
 
+    print(f'loading BillOfLading')
     BillOfLading.load_from_csv(ROOT_DIR / 'scripts/bol-10000.csv')
+    print(f'loading Address')
     Address.load_from_csv(ROOT_DIR / 'spreadsheet_data/da-base-OLTP - Address.csv')
+    print(f'loading BusinessEntity')
     BusinessEntity.load_from_csv(ROOT_DIR / 'spreadsheet_data/da-base-OLTP - BusinessEntity.csv')
+    print(f'loading Commodity')
     Commodity.load_from_csv(ROOT_DIR / 'spreadsheet_data/da-base-OLTP - Commodity.csv')
+    print(f'loading Container')
     Container.load_from_csv(ROOT_DIR / 'spreadsheet_data/da-base-OLTP - Container.csv')
+    print(f'loading ContainerModel')
     ContainerModel.load_from_csv(ROOT_DIR / 'spreadsheet_data/da-base-OLTP - ContainerModel.csv')
+    print(f'loading Port')
     Port.load_from_csv(ROOT_DIR / 'spreadsheet_data/da-base-OLTP - Port.csv')
+    print(f'loading Vehicle')
     Vehicle.load_from_csv(ROOT_DIR / 'spreadsheet_data/da-base-OLTP - Vehicle.csv')
 
     if dataset is not None:
