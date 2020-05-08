e7019426c0ef537915529439b1db7521bec8b2d2
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-08 20:02:10 +0700

    Cast mph speed for Vehicle

--- sql_scripts/etl_load_vehicle.sql
+++ sql_scripts/etl_load_vehicle.sql
@@ -4,7 +4,7 @@ SELECT [Vehicle Key],
        [Vehicle Name],
        [Vehicle Capacity],
        [Vehicle Speed (km/h)],
-       [Vehicle Speed (km/h)] * 0.6       [Vehicle Speed (mile/h)],
+       CAST(ROUND([Vehicle Speed (km/h)] * 0.6,3) AS decimal(18,3))       [Vehicle Speed (mile/h)],
        [Vehicle Builder],
        [Vehicle Fuel Usage per Day] * 400 [Fuel cost per day]
 FROM Vehicle V
