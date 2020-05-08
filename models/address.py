0b7133ea20a1cf87e3ebda17ae34ec6166fc1601
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-08 15:05:56 +0700

    Update Address model dbtypes + Add port init hook

--- models/address.py
+++ models/address.py
@@ -16,29 +16,35 @@ class Address(BaseModel):
         'City',
         'Country',
         ('Alpha 2', None, 'nvarchar(2)'),
-        ('ZIP Code', None, 'nvarchar(12)'),
-        ('Latitude', None, 'nvarchar(15)'),
-        ('Longitude', None, 'nvarchar(15'),
+        ('ZIP Code', None, 'nvarchar(20)'),
+        ('Latitude', None, 'nvarchar(20)'),
+        ('Longitude', None, 'nvarchar(20)'),
     )
 
-    address_line_1: str = ''
-    address_line_2: str = ''
-    city: str = ''
-    country: str = ''
-    alpha_2: str = ''
-    zip_code: str = ''
-    latitude: str = ''
-    longitude: str = ''
+    address_line_1: str = None
+    address_line_2: str = None
+    city: str = None
+    country: str = None
+    alpha_2: str = None
+    zip_code: str = None
+    latitude: str = None
+    longitude: str = None
 
     def __post_init__(self):
-        self.address_line_1 = self.address_line_1.strip()
-        self.address_line_2 = self.address_line_2.strip()
-        self.city = self.city.strip()
+        if self.address_line_1:
+            self.address_line_1 = self.address_line_1.strip()
+        if self.address_line_2:
+            self.address_line_2 = self.address_line_2.strip()
+        if self.city:
+            self.city = self.city.strip()
         self.country = self.country.strip()
         self.alpha_2 = self.alpha_2.strip()
-        self.zip_code = self.zip_code.strip()
-        self.latitude = self.latitude.strip()
-        self.longitude = self.longitude.strip()
+        if self.zip_code:
+            self.zip_code = self.zip_code.strip()
+        if self.latitude:
+            self.latitude = self.latitude.strip()
+        if self.longitude:
+            self.longitude = self.longitude.strip()
 
     @property
     def address_key(self) -> int:
