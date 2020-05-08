a4ad816a4ba36c3bbe9cb6814163354ab08fbe6f
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-08 14:29:26 +0700

    Add post init hook + update dbtype to match database

--- models/address.py
+++ models/address.py
@@ -15,10 +15,10 @@ class Address(BaseModel):
         'Address Line 2',
         'City',
         'Country',
-        'Alpha 2',
-        'ZIP Code',
-        'Latitude',
-        'Longitude',
+        ('Alpha 2', None, 'nvarchar(2)'),
+        ('ZIP Code', None, 'nvarchar(12)'),
+        ('Latitude', None, 'nvarchar(15)'),
+        ('Longitude', None, 'nvarchar(15'),
     )
 
     address_line_1: str = ''
@@ -29,6 +29,16 @@ class Address(BaseModel):
     zip_code: str = ''
     latitude: str = ''
     longitude: str = ''
+
+    def __post_init__(self):
+        self.address_line_1 = self.address_line_1.strip()
+        self.address_line_2 = self.address_line_2.strip()
+        self.city = self.city.strip()
+        self.country = self.country.strip()
+        self.alpha_2 = self.alpha_2.strip()
+        self.zip_code = self.zip_code.strip()
+        self.latitude = self.latitude.strip()
+        self.longitude = self.longitude.strip()
 
     @property
     def address_key(self) -> int:
