d58934e9a6816bc3b011721dbfbb04ca1829259f
Punyawee Srithonhkerd <tonkla9874@gmail.com>
2020-05-08 15:02:04 +0700

    add db type for container model

--- models/container_model.py
+++ models/container_model.py
@@ -7,13 +7,32 @@ from .base import BaseModel
 class ContainerModel(BaseModel):
     _instances = []
     fields = (
-        ('Container Model Key', None, 'int'), 'ISO Size Code', 'ISO Type Code', 'Model Description', 'Owner Code',
-        'Serial Number Range Start', 'Serial Number Range End', 'Inside Length (mm)', 'Inside Width (mm)',
-        'Inside Height (mm)', 'Inside Middle Height (mm)', 'Inside Side Height (mm)', 'Max stow Height',
-        'Roof Opening Length (mm)', 'Roof Opening Width (mm)', 'Door Opening Width (mm)',
-        'Door Opening Height (mm)', 'Door Opening Width C (mm)', 'Door Opening Width D (mm)',
-        'Door Opening Width B (mm)', 'Door Opening Height E (mm)', 'Door Opening Height F (mm)',
-        'Max Gross Weight (kg)', 'Tare Weight (kg)', 'Max Payload Weight (kg)', 'Capacity (cbm)'
+        ('Container Model Key', None, 'int'),
+        ('ISO Size Code', None, 'nvarchar(2)'),
+        ('ISO Type Code', None, 'nvarchar(2)'),
+        ('Model Description', None, 'nvarchar(255)'),
+        ('Owner Code', None, 'nvarchar(4)'),
+        ('Serial Number Range Start', None, 'int'),
+        ('Serial Number Range End', None, 'int'),
+        ('Inside Length (mm)', None, 'int'),
+        ('Inside Width (mm)', None, 'int'),
+        ('Inside Height (mm)', None, 'int'),
+        ('Inside Middle Height (mm)', None, 'int'),
+        ('Inside Side Height (mm)', None, 'int'),
+        ('Max stow Height', None, 'int'),
+        ('Roof Opening Length (mm)', None, 'int'),
+        ('Roof Opening Width (mm)', None, 'int'),
+        ('Door Opening Width (mm)', None, 'int'),
+        ('Door Opening Height (mm)', None, 'int'),
+        ('Door Opening Width C (mm)', None, 'int'),
+        ('Door Opening Width D (mm)', None, 'int'),
+        ('Door Opening Width B (mm)', None, 'int'),
+        ('Door Opening Height E (mm)', None, 'int'),
+        ('Door Opening Height F (mm)', None, 'int'),
+        ('Max Gross Weight (kg)', None, 'int'),
+        ('Tare Weight (kg)', None, 'int'),
+        ('Max Payload Weight (kg)', None, 'int'),
+        ('Capacity (cbm)', None, 'decimal')
     )
 
     container_model_key: int
