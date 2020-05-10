a2eaf2d250ebfd608fe24c33e76dccd39ce3478f
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-10 21:27:28 +0700

    Rename HS code files

---
+++ sql_scripts/create_hs_code.sql
@@ -0,0 +1,14 @@
+USE FreightShipping;
+
+IF OBJECT_ID('dbo.HarmonizedSystem', 'U') IS NOT NULL
+    DROP TABLE HSCode;
+GO
+
+CREATE TABLE HarmonizedSystem
+(
+    section     nvarchar(5)    NOT NULL,
+    hscode      nvarchar(6)    NOT NULL,
+    description nvarchar(255) NOT NULL,
+    parent      nvarchar(5)    NOT NULL,
+    level       tinyint       NOT NULL,
+);
