8dd7b746866b8fc25372bac7d6ad1106d27eeaee
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-20 23:02:26 +0700

    Fix Address

--- sql_scripts/etl_load_business_entity.sql
+++ sql_scripts/etl_load_business_entity.sql
@@ -1,14 +1,14 @@
 USE FreightShipping;
 
 SELECT [Business Entity Key],
-       [Name]                                                                    [Business Entity Name],
-       [Phone]                                                                   [Business Entity Telephone],
-       [Fax]                                                                     [Business Entity Fax],
-       [E-mail]                                                                  [Business Entity E-mail],
-       CONCAT([Address Line 1], [Address Line 2], [City], [Country], [ZIP Code]) [Business Entity Address],
-       [City]                                                                    [Business Entit City],
-       [Country]                                                                 [Business Entity Country],
-       [ZIP code]                                                                [Business Entity ZIP Code]
+       [Name]                                                                            [Business Entity Name],
+       [Phone]                                                                           [Business Entity Telephone],
+       [Fax]                                                                             [Business Entity Fax],
+       [E-mail]                                                                          [Business Entity E-mail],
+       CONCAT_WS(' ', [Address Line 1], [Address Line 2], [City], [Country], [ZIP Code]) [Business Entity Address],
+       [City]                                                                            [Business Entity City],
+       [Country]                                                                         [Business Entity Country],
+       [ZIP code]                                                                        [Business Entity ZIP Code]
 FROM BusinessEntity BE
          JOIN Address A ON BE.[Address Key] = A.[Address Key]
 ORDER BY [Business Entity Key]
--- sql_scripts/etl_load_port.sql
+++ sql_scripts/etl_load_port.sql
@@ -3,10 +3,10 @@ USE FreightShipping;
 SELECT [Port Key],
        [Port Name],
        [Port Type],
-       [Country]                                                                 [Port Country],
-       [City]                                                                    [Port City],
-       CONCAT([Address Line 1], [Address Line 2], [City], [Country], [ZIP Code]) [Address],
-       P.Phone                                                                   [Port Telephone]
+       [Country]                                                                         [Port Country],
+       [City]                                                                            [Port City],
+       CONCAT_WS(' ', [Address Line 1], [Address Line 2], [City], [Country], [ZIP Code]) [Port Address],
+       P.Phone                                                                           [Port Telephone]
 FROM Port P
          JOIN Address A on P.[Address Key] = A.[Address Key]
 ORDER BY [Port Key]
