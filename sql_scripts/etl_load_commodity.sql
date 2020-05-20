24bdb876f33a5a90698eafabdfa2865d5e299fbc
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-21 00:21:32 +0700

    Add HS Code hierachy for cotainer dim

--- sql_scripts/etl_load_commodity.sql
+++ sql_scripts/etl_load_commodity.sql
@@ -1,11 +1,19 @@
 SELECT [Commodity Key],
        [HS Code],
-       C.[Description]  [Commodity Description],
-       HSS.name       [Category Description],
-       HS.description [Subcategory Description],
+       HS6.hscode      [Sub Heading],
+       HS6.description [Sub Heading Description],
+       HS4.hscode      [Heading],
+       HS4.description [Heading Description],
+       HS2.hscode      [Chapter],
+       HS2.description [Chapter Description],
+       HSS.section     [Section],
+       HSS.name        [Section Name],
+       C.[Description] [Commodity Description],
        [Package Size (m^3)],
        [Package Weight (kg)]
 FROM FreightShipping.dbo.Commodity C
-         JOIN FreightShippingETL.dbo.HSCode HS on C.[HS Code] = HS.hscode
-         JOIN FreightShippingETL.dbo.HSSection HSS on HS.section = HSS.section
+         JOIN FreightShippingETL.dbo.HSCode HS6 on SUBSTRING(C.[HS Code], 1, 6) = HS6.hscode -- Sub Heading
+         JOIN FreightShippingETL.dbo.HSCode HS4 on HS6.parent = HS4.hscode -- Heading
+         JOIN FreightShippingETL.dbo.HSCode HS2 on HS4.parent = HS2.hscode -- Chapter
+         JOIN FreightShippingETL.dbo.HSSection HSS on HS6.section = HSS.section
 ORDER BY [Commodity Key];
