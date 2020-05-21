6da93ae4854c74f864da9f0abb80ec1cde408a30
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-21 23:25:46 +0700

    Fix error created by incomplete data

--- sql_scripts/etl_load_business_entity.sql
+++ sql_scripts/etl_load_business_entity.sql
@@ -1,14 +1,26 @@
-SELECT [Business Entity Key],
-       [Name]                                                                            [Business Entity Name],
-       [Phone]                                                                           [Business Entity Telephone],
-       [Fax]                                                                             [Business Entity Fax],
-       [E-mail]                                                                          [Business Entity E-mail],
-       CONCAT_WS(' ', [Address Line 1], [Address Line 2], [City], [Country], [ZIP Code]) [Business Entity Address],
-       CACC.Continent_Name                                                               [Business Entity Region],
-       [City]                                                                            [Business Entity City],
-       [Country]                                                                         [Business Entity Country],
-       [ZIP code]                                                                        [Business Entity ZIP Code]
-FROM BusinessEntity BE
+WITH CountryContinent (Two_Letter_Country_Code, continent_continent)
+         AS (
+        SELECT m.Two_Letter_Country_Code,
+               continent_continent = STUFF((SELECT '/' + md.Continent_Name
+                                            FROM FreightShippingETL.dbo.CountryAndContinentCodes md
+                                            WHERE m.Two_Letter_Country_Code = md.Two_Letter_Country_Code
+                                            FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 1, '')
+        FROM FreightShippingETL.dbo.CountryAndContinentCodes m
+                 JOIN FreightShippingETL.dbo.CountryAndContinentCodes s
+                      ON s.Two_Letter_Country_Code = m.Two_Letter_Country_Code
+        GROUP BY m.Two_Letter_Country_Code
+    )
+SELECT [Business Entity Key]
+       ,[Name]                                                                            [Business Entity Name]
+       ,[Phone]                                                                           [Business Entity Telephone]
+       ,[Fax]                                                                             [Business Entity Fax]
+       ,[E-mail]                                                                          [Business Entity E-mail]
+       ,CONCAT_WS(' ', [Address Line 1], [Address Line 2], [City], [Country], [ZIP Code]) [Business Entity Address]
+       ,CC.continent_continent                                                            [Business Entity Region]
+       ,[City]                                                                            [Business Entity City]
+       ,[Country]                                                                         [Business Entity Country]
+       ,[ZIP code]                                                                        [Business Entity ZIP Code]
+FROM FreightShipping.dbo.BusinessEntity BE
          JOIN FreightShipping.dbo.Address A ON BE.[Address Key] = A.[Address Key]
-         JOIN FreightShippingETL.dbo.CountryAndContinentCodes CACC ON A.[Alpha 2] = CACC.Two_Letter_Country_Code
+         LEFT JOIN CountryContinent CC ON A.[Alpha 2] = CC.Two_Letter_Country_Code
 ORDER BY [Business Entity Key]
--- sql_scripts/etl_load_port.sql
+++ sql_scripts/etl_load_port.sql
@@ -1,13 +1,24 @@
+WITH CountryContinent (Two_Letter_Country_Code, continent_continent)
+         AS (
+        SELECT m.Two_Letter_Country_Code,
+               continent_continent = STUFF((SELECT '/' + md.Continent_Name
+                                            FROM FreightShippingETL.dbo.CountryAndContinentCodes md
+                                            WHERE m.Two_Letter_Country_Code = md.Two_Letter_Country_Code
+                                            FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 1, '')
+        FROM FreightShippingETL.dbo.CountryAndContinentCodes m
+                 JOIN FreightShippingETL.dbo.CountryAndContinentCodes s
+                      ON s.Two_Letter_Country_Code = m.Two_Letter_Country_Code
+        GROUP BY m.Two_Letter_Country_Code
+    )
 SELECT [Port Key],
        [Port Name],
        [Port Type],
-       CACC.Continent_Name                                                               [Port Region],
+       CC.continent_continent                                                            [Port Region],
        [Country]                                                                         [Port Country],
        [City]                                                                            [Port City],
        CONCAT_WS(' ', [Address Line 1], [Address Line 2], [City], [Country], [ZIP Code]) [Port Address],
        P.Phone                                                                           [Port Telephone]
 FROM Port P
          JOIN FreightShipping.dbo.Address A ON P.[Address Key] = A.[Address Key]
-         JOIN FreightShippingETL.dbo.CountryAndContinentCodes CACC ON A.[Alpha 2] = CACC.Two_Letter_Country_Code
+         LEFT JOIN CountryContinent CC ON A.[Alpha 2] = CC.Two_Letter_Country_Code
 ORDER BY [Port Key]
-
