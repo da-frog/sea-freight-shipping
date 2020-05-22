6ab29747b8e1387a81e86a3b411cd71a1c2bee39
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-05-22 22:48:30 +0700

    Make script write to desired directory directly

--- scripts/insert_date_dim.py
+++ scripts/insert_date_dim.py
@@ -1,6 +1,8 @@
 from datetime import timedelta
 from models.date import DateDimension, Date
 
+from dirs import ROOT_DIR
+
 date = Date(2016, 1, 1)
 for _ in range(2000):
     DateDimension(date)
@@ -9,4 +11,4 @@ for _ in range(2000):
     #     break
 print(date)
 
-DateDimension.dump_to_sql('date_dim.sql')
+DateDimension.dump_to_sql(ROOT_DIR / 'sql_scripts/insert_date.sql')
